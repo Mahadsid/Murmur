db11b747529242b73fd8eaf61a6a077622d9bdeb
MSID <muhammadmahad0808@gmail.com>
2025-12-22 18:46:46 +0530

    All AI features done

--- app/middlewares/arcjet/heavy-write.ts
+++ app/middlewares/arcjet/heavy-write.ts
@@ -1,6 +1,6 @@
 // Heavy Write operation which is creating a workspace so to protect that we use rate limiting 2 req per min.
 
-import arcjet, { detectBot, shield, slidingWindow } from "@/lib/arcjet"
+import arcjet, { sensitiveInfo, slidingWindow } from "@/lib/arcjet"
 import { base } from "../base";
 import { KindeUser } from "@kinde-oss/kinde-auth-nextjs";
 
@@ -13,7 +13,13 @@ const buildStandardAj = () =>
             interval: "1m",
             max: 2,
         })
-    );
+    )
+        .withRule(
+            sensitiveInfo({
+                mode: "LIVE",
+                deny: ["PHONE_NUMBER", "CREDIT_CARD_NUMBER", "EMAIL", "IP_ADDRESS"]
+            })
+    )
 
 export const heavyWriteSecurityMiddleware = base.$context<{
     request: Request;
@@ -24,6 +30,11 @@ export const heavyWriteSecurityMiddleware = base.$context<{
     });
 
     if (decision.isDenied()) {
+        if (decision.reason.isSensitiveInfo()) {
+            throw errors.BAD_REQUEST({
+                message: 'Sensitive information detected. Please remove PII (eg. credit card, phone number, email, IP address.'
+            });
+        }
         if (decision.reason.isRateLimit()) {
             throw errors.RATE_LIMITED({
                 message: 'Too many requests. Please keep waiting time in each request.'
